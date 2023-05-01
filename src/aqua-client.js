a5b55d47d1128e4e4a92e1407dff7151b8bd182b
Timothy M. Crider <timcrider@gmail.com>
2023-05-01 19:34:09 +0000

    Adding some client functionality

--- src/aqua-client.js
+++ src/aqua-client.js
@@ -1,6 +1,8 @@
 const https = require('https');
 const querystring = require('querystring');
 const credentials = require('./credentials');
+const { URL } = require('node:url');
+const { get } = require('http');
 
 class AquaClient {
   constructor(args={}) {
@@ -15,7 +17,12 @@ class AquaClient {
     // Authentication token
     this._token = new credentials();
     Object.defineProperty(this, "_token", {enumerable: false, writable: false});
-  }
+
+    // Options
+    this._options = {
+      per_page_max: 1000
+    };
+  };
 
   /**
    * Set Aqua URL instance
@@ -27,7 +34,7 @@ class AquaClient {
     }
 
     this._instance = instance;
-  }
+  };
 
   /**
    * Set the token
@@ -37,23 +44,79 @@ class AquaClient {
     Object.defineProperty(this, "_token", {enumerable: false, writable: true});
     this._token.store(token);
     Object.defineProperty(this, "_token", {enumerable: false, writable: false});
-  }
+  };
 
-
-  request() {
-
-  }
+  request() {};
+  get() {};
+  post() {};
 
   /**
-   * Convert key/value pair object to querystring
    * 
-   * @param {*} qs Convert key/value pair object
+   * @param {string} path 
+   * @param {object} qs 
+   * @param {number} currentPage 
+   * @param {number} perPage 
    * @returns 
    */
-  querystring(qs) {
-    return (typeof qs === 'object') ? `?${querystring.stringify(qs)}` : '';
-  }
+  getPage(path, qs={}, currentPage=1, perPage=1000) {
+    if (perPage > this._options.per_page_max) {
+      throw new Error(`perPage must be less than or equal to ${this._options.per_page_max}`);
+    }
 
+    return this.get(path, {...qs, ...{page: currentPage, pagesize: perPage}});
+  };
+
+  /**
+   * Fetch all data from an aqua endpoint
+   * @param {string} path 
+   * @param {object} qs 
+   * @returns 
+   */
+  getAll(path="", qs={}) {
+    // @todo Instead of doing this synchronously, we should do it asynchronously
+    return new Promise(async (resolve, reject) => {
+      let first = await this.getPage(path, qs, 1, 1);
+
+      // If we have less than the max per page, we can just fetch and return the first page
+      if (first.count <= this._options.per_page_max) {
+        let all = await this.getPage(path, qs, 1, this._options.per_page_max)
+        resolve(all);
+        return;
+      }
+
+      let pagesNeeded = Math.ceil(first.count / this._options.per_page_max);
+      let all = [];
+
+      for (let i = 1; i <= pagesNeeded; i++) {
+        let page = await this.getPage(path, qs, i, this._options.per_page_max);
+        if (page.result) {
+          all = [...all, ...page.result];
+//          all = all.concat(page.result);
+        }
+      }
+
+      resolve(all);
+    });
+  };
+
+  /**
+   * Build URL
+   * @param {string} path uri path
+   * @param {object} qs object key/value pair querystring
+   * @returns string url
+   */
+  buildUrl(path="", qs={}) {
+    const url = new URL(this._instance);
+    url.pathname = path;
+
+    if (typeof qs === 'object' && Object.keys(qs).length > 0) {
+      for (const [key, value] of Object.entries(qs)) {
+        url.searchParams.append(key, value);
+      }
+    }
+
+    return url.toString();
+  }
 }
 
 module.exports = AquaClient;
